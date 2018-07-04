88aa0e0bb29d879b55be93eb7139f2829cbfe125
Nicolás Gomez <40768045+NicolasAlejandroGomezEchenique@users.noreply.github.com>
2018-07-04 12:05:04 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,11 @@
-function mostrar()
+function Mostrar()
 {
-  
+  var nombre,localidad;
+  nombre=document.getElementById('elNombre').value;
+ 
+
+  localidad=document.getElementById('laLocalidad').value;
+
+  alert("usted es "+nombre+" y vive en la localidad de "+localidad)
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,10 @@
 
-function mostrar()
+function Mostrar()
 {
-
+ var base,altura,resultado;
+ base=prompt("base");
+ base=parseInt(base) 
+ altura=prompt("altura");
+ resultado=base*3
+ alert(resultado);
 }
